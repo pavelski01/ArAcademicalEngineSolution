30501cef56a4f31e8ede8562e6b6e66bf89da9da
pavelski01 <pavelski01@gmail.com>
2022-05-23 17:15:17 +0200

    Reinit

---
+++ Assets/Scripts/CelObrazuZachowanie.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CelObrazuZachowanie : DefaultObserverEventHandler
+{
+    protected override void OnTrackingFound()
+    {
+        var eventData = new ZdarzenieRrDto
+        {
+            objekt = transform.gameObject,
+            nazwa = "znalezionoCelObrazu"
+        };
+        Debug.Log(nameof(OnTrackingFound) + $":{eventData.nazwa}");
+        NadzorcaZdarzen.WyzwolZdarzenie(eventData);
+    }
+    protected override void OnTrackingLost()
+    {
+        var eventData = new ZdarzenieRrDto
+        {
+            objekt = transform.gameObject,
+            nazwa = "straconoCelObrazu"
+        };
+        Debug.Log(nameof(OnTrackingLost) + $":{eventData.nazwa}");
+        NadzorcaZdarzen.WyzwolZdarzenie(eventData);
+    }
+}
---
+++ Assets/Scripts/NadzorcaZdarzen.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
+using Assets.Scripts;
+
+public class NadzorcaZdarzen : MonoBehaviour
+{   
+    private static NadzorcaZdarzen kierownikZdarzen;
+
+    private Dictionary<string, ZdarzenieRr> SlownikZdarzen { get; set; }
+    public DostepnoscKamery DostepnoscKamery { get; set; }
+    public static NadzorcaZdarzen Instancja 
+    {
+        get 
+        {
+            if (!kierownikZdarzen) 
+            {
+                kierownikZdarzen = (NadzorcaZdarzen)FindObjectOfType(typeof(NadzorcaZdarzen));
+                if (!kierownikZdarzen) 
+                    Debug.Log("Musi być co najmniej jeden KierownikZdarzen");
+                else kierownikZdarzen.Inicjalizuj();                
+            }
+            return kierownikZdarzen;
+        }
+    }
+    
+    void Inicjalizuj()
+    {
+        if (SlownikZdarzen == null)
+            SlownikZdarzen = new Dictionary<string, ZdarzenieRr>();
+        if (DostepnoscKamery == null)
+            DostepnoscKamery = gameObject.AddComponent<DostepnoscKamery>();
+    }
+    public static void RozpocznijNasluch(string nazwaZdarzenia, UnityAction<ZdarzenieRrDto> nasluchiwacz) 
+    {
+        if (Instancja.SlownikZdarzen.TryGetValue(nazwaZdarzenia, out ZdarzenieRr zdarzenie))
+            zdarzenie?.AddListener(nasluchiwacz);
+        else
+        {
+            zdarzenie = new ZdarzenieRr();
+            zdarzenie.AddListener(nasluchiwacz);
+            Instancja.SlownikZdarzen.Add(nazwaZdarzenia, zdarzenie);
+        }
+    }
+    public static void ZakonczNasluch(string nazwaZdarzenia, UnityAction<ZdarzenieRrDto> nasluchiwacz)
+    {
+        if (Instancja == null || nasluchiwacz == null)
+            return;
+        if (Instancja.SlownikZdarzen.TryGetValue(nazwaZdarzenia, out ZdarzenieRr zdarzenie))
+            zdarzenie?.RemoveListener(nasluchiwacz);
+    }
+    public static void WyzwolZdarzenie(ZdarzenieRrDto zdarzenie) 
+    {
+        OdpluskwiaczDziennik.WpisDziennika("Zdarzenie Rozgloszone: " + zdarzenie.nazwa);
+        if (Instancja.SlownikZdarzen.TryGetValue(zdarzenie.nazwa, out ZdarzenieRr zarejestrowanaAkcja))
+            zarejestrowanaAkcja?.Invoke(zdarzenie);
+    }    
+}
